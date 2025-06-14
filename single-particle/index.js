67988705d6b84f564ce6843425bfd8a513f7df61
Ademsk1 <adam.illoul24@gmail.com>
2025-06-14 17:58:32 +0100

    feat: field lines@

--- single-particle/index.js
+++ single-particle/index.js
@@ -2,41 +2,13 @@ import * as THREE from 'three'
 import { OrbitControls } from 'jsm/controls/OrbitControls.js';
 
 
-let type = "Sinusoidal"
 
-const form = document.querySelector("form")
-const log = document.querySelector("#log")
-
-form.addEventListener(
-  "submit",
-  (event) => {
-    event.preventDefault();
-
-    const data = new FormData(form);
-    let output = "";
-    console.log({data})
-    for (const entry of data) {
-      output = `${entry[1]}\r`;
-    }
-    log.innerText = output
-    console.log(output)
-    type = output.trim()
-
-  },
-  false,
-);
-
-
-const f = (x) => {
-    efield = getElectricField()
-
-}
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
     75, window.innerWidth / window.innerHeight, 0.1, 1000
 );
-const hemiLight = new THREE.HemisphereLight(0xffff44, 0xffffff)
+const hemiLight = new THREE.HemisphereLight(0xffff44, 0x00ffff)
 scene.add(hemiLight)
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 new OrbitControls(camera, renderer.domElement);
@@ -46,65 +18,116 @@ document.body.appendChild(renderer.domElement);
 
 const axesHelper = new THREE.AxesHelper(5);
 scene.add(axesHelper);
-
-const width = 10
-const height = 10
-const segmentsX = 100
-const segmentsY = 100
-
-const geometry = new THREE.PlaneGeometry(width, height, segmentsX,segmentsY )
-const material = new THREE.MeshBasicMaterial({color: 0x777777, wireframe: true,})
+camera.position.z = 10
 
 
 
-const group = newShape()
+const particle = new THREE.Mesh(
+    new THREE.IcosahedronGeometry(1, 5),
+    new THREE.MeshStandardMaterial({color: 0xa405b0, flatShading: true})
+)
 
-scene.add(group)
 
+const arrowsOut = (origin, number, length=2, type="arrow") => {
+    let dir
+    let phi
+    let theta
+    const material = new THREE.LineDashedMaterial( {
+        color: 0xffffff,
+        linewidth: 1,
+        scale: 1,
+        dashSize: 3,
+        gapSize: 1,
+    });
+    const group = new THREE.Group()
+    for (let i =0;i<number;i++) {
+         phi = (2*Math.PI / number) * i 
+         for (let j=0;j<number;j++) {
+            theta = (Math.PI / number) * j
+            dir = new THREE.Vector3(
+                Math.sin(theta) * Math.sin(phi),
+                Math.sin(theta) * Math.cos(phi),
+                Math.cos(theta)
+                )
+            let arr
+            if (type === "arrow") {
+                arr = new THREE.ArrowHelper(dir, origin, length, 0x999999)
+            }
+            else {
 
-const getElectricField = () => {
-    if (type === "Sinusoidal") {
-        return [0,1]
+                arr = new THREE.Line(new THREE.BufferGeometry().setFromPoints(
+                   [ origin, dir.multiplyScalar(100)]), material
+                )
+            }
+            arr.userData.direction = dir
+            group.add(
+            arr
+        )
+         }
     }
-    console.log(type.toLowerCase())
-    switch (type.toLowerCase()) {
-        case "sinusoidal":
-            return [0,1]
-        case "linear": 
-            return [1,0]
-        default:
-            return [0,0]
-    }
-}
+    return group
 
-const line = new THREE.Mesh( geometry, material );
-scene.add( line );
 
-camera.position.z = 3;
-const electronPosition = group.children[0].position 
-const protonPosition = group.children[1].position
-console.log({electronPosition})
-console.log(group)
+}   
+
+const arrs = []
+for (let i=0;i<10;i++) {
+    arrs.push(arrowsOut(particle.position, 5))
+} 
+const MAX_RADIUS = 10
+let arrows = arrowsOut(particle.position, 5)
+console.log({arrows})
+let constantArrows = arrowsOut(particle.position, 5, MAX_RADIUS, "line")
+arrs.forEach((_, i)=> {
+    scene.add(arrs[i])
+})
+scene.add(constantArrows)
+
+scene.add(particle)
 // Animation loop
+renderer.render(scene, camera);
 
 function animate(t=0) {
-    const efield = getElectricField()
-    console.log(efield)
+    arrs.forEach((arrowGroup,i) => {
+        const pos = arrowGroup.children[0].position
+        const r = Math.sqrt(pos.x**2 + pos.y**2 + pos.z **2)
+        if (r > MAX_RADIUS) {
+            arrowGroup.children.forEach((arrow)=> {
+            arrow.position.set(0,0,0)
+            })
+        }
+        console.log(t)
+        const modifier = t > i*0.2
+        arrowGroup.children.forEach((arrow)=> {
+            if (modifier) {
+            const {x,y,z} = arrow.position
+            const {x:xd, y:yd, z:zd} = arrow.userData.direction
+            arrow.position.set(
+                x + xd * 0.01,
+                y + yd * 0.01,
+                z + zd * 0.01
+            )
+            }
+
+            // arrow.position.x += arrow.userData.direction.x * 0.01 * modifier
+            // arrow.position.y += arrow.userData.direction.y * 0.01 * modifier
+            // arrow.position.z += arrow.userData.direction.z * 0.01 * modifier
+        })
+    })
+    // arrows.children.forEach((arrow)=> {
+    //     if (r > 5) {
+    //         arrow.position.x = 0
+    //         arrow.position.y = 0
+    //         arrow.position.z = 0
+    //     }
+    //     arrow.position.x += arrow.userData.direction.x * 0.01
+    //     arrow.position.y += arrow.userData.direction.y * 0.01
+    //     arrow.position.z += arrow.userData.direction.z * 0.01
+    // })
     requestAnimationFrame(()=> animate(t));
-    const p = geometry.attributes.position
-    for (let i=0;i<p.count;i++) {
-        let x = p.getX(i)
-        let y = p.getY(i)
-        const z = Math.sin((x) -t*3)*efield[1] + x*efield[0]
-        p.setZ(i, z)
-        
-    }
-    electronPosition.z = Math.sin(electronPosition.x - t*3)
-    protonPosition.z = Math.sin(protonPosition.x - t*3)
     t+=0.001
-    p.needsUpdate = true
-    
     renderer.render(scene, camera);
+    
 }
 const t = 0
 animate(t);
